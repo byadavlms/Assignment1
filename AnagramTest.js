7ea1fb1b7dd8b442a18fb4684c811a1d2fdcd297
ADMIN1 <bhupendra.yadav@lmsin.com>
2022-02-21 17:19:07 +0530

    Anagram Test File

---
+++ AnagramTest.js
@@ -0,0 +1,16 @@
+function isAnagram(string1, string2) {
+    let len1 = string1.length;
+    let len2 = string2.length;
+    if (string1.length !== string2.length) {
+        return false;
+    }
+
+    const str1 = string1.split('').sort().join('');
+    const str2 = string2.split('').sort().join('');
+    if (str1 === str2) {
+        return true;
+    }
+    return false;
+}
+
+isAnagram('cellar', 'recall');
